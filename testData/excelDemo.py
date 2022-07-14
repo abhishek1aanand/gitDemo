9011bfe6be0e9bd1e71d9b34687c609ffc6e863f
Abhishek <abhishek1aanand@gmail.com>
2022-07-14 16:26:15 +0530

    American Architect Developer

--- testData/excelDemo.py
+++ testData/excelDemo.py
@@ -2,7 +2,7 @@ import openpyxl
 
 book = openpyxl.load_workbook("C:\\Users\\Abhishek\\PRACTISE\\Selenium\\PythonSelFramework\\Book1.xlsx")
 sheet = book.active
-Dict = {}
+Dict1 = {}
 cell = sheet.cell(row=1, column=2)
 print(cell.value)
 sheet.cell(row=2, column=2).value = "Rahul"
@@ -16,4 +16,4 @@ for i in range(1, sheet.max_row + 1):
 
         for j in range(2, sheet.max_column + 1):
             Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
-print(Dict)
+print(Dict1)
